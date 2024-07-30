a069c81822e8a6796ccb64e88d3466bf9db35c6a
MalicknND <ndiayemalick062@gmail.com>
2024-07-31 00:20:06 +0200

    chore: Add Categories component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
 import Featured from "../../components/Featured/Featured";
+import Categories from "../../components/Categories/Categories";
 const Home = () => {
   return (
     <div className="home">
       <Slider />
       <Featured type="featured" />
       <Featured type="trending" />
+      <Categories />
     </div>
   );
 };
