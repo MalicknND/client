29a7b0b3c5affee5ac131900955b369b9cf94e3b
MalicknND <ndiayemalick062@gmail.com>
2024-07-31 00:40:44 +0200

    chore: Update styles and fix overflow issues in Slider and Contact components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
 import Featured from "../../components/Featured/Featured";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 const Home = () => {
   return (
     <div className="home">
@@ -10,6 +11,7 @@ const Home = () => {
       <Featured type="featured" />
       <Featured type="trending" />
       <Categories />
+      {/* <Contact /> */}
     </div>
   );
 };
